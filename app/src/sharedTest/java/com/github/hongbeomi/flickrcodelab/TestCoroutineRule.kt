f6af3358fd05a20bee629c20aba590e4dc4c6082
hongbeom <dks3963@gmail.com>
2022-05-08 22:07:50 +0900

    write TestCoroutineRule

---
+++ app/src/sharedTest/java/com/github/hongbeomi/flickrcodelab/TestCoroutineRule.kt
@@ -0,0 +1,20 @@
+package com.github.hongbeomi.flickrcodelab
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class TestCoroutineRule(
+    private val dispatcher: TestDispatcher = StandardTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description?) {
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        Dispatchers.resetMain()
+    }
+}
