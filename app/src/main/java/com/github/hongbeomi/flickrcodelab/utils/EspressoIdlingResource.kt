e69c71750b497c1be3e87af985d8a1f6dd6253d8
hongbeom <dks3963@gmail.com>
2022-05-08 22:13:40 +0900

    EspressoIdlingResource

---
+++ app/src/main/java/com/github/hongbeomi/flickrcodelab/utils/EspressoIdlingResource.kt
@@ -0,0 +1,36 @@
+package com.github.hongbeomi.flickrcodelab.utils
+
+import androidx.test.espresso.idling.CountingIdlingResource
+
+object EspressoIdlingResource {
+
+    private const val RESOURCE = "GLOBAL"
+
+    @JvmField
+    val countingIdlingResource = CountingIdlingResource(RESOURCE)
+
+    fun increment() {
+
+        countingIdlingResource.increment()
+    }
+
+    fun decrement() {
+        if (!countingIdlingResource.isIdleNow) {
+            countingIdlingResource.decrement()
+        }
+    }
+}
+
+inline fun <T> wrapEspressoIdlingResource(isUsing: Boolean, function: () -> T): T {
+    if (!isUsing) {
+        return function()
+    }
+    // Espresso does not work well with coroutines yet. See
+    // https://github.com/Kotlin/kotlinx.coroutines/issues/982
+    EspressoIdlingResource.increment() // Set app as busy.
+    return try {
+        function()
+    } finally {
+        EspressoIdlingResource.decrement() // Set app as idle.
+    }
+}
