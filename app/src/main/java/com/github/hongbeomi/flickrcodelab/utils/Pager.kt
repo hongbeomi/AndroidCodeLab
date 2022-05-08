4b436771aa6d2a4d7fe14e4f5034a9fbb72068d9
hongbeom <dks3963@gmail.com>
2022-05-08 22:16:43 +0900

    create Custom Pager

--- app/src/main/java/com/github/hongbeomi/flickrcodelab/utils/Pager.kt
+++ app/src/main/java/com/github/hongbeomi/flickrcodelab/utils/Pager.kt
@@ -1,4 +1,27 @@
 package com.github.hongbeomi.flickrcodelab.utils
 
 class Pager {
+
+    private var pageIndex = START_PAGE_INDEX
+
+    suspend fun load(predicate: suspend (Int) -> Boolean?) {
+        val isSuccess = predicate(pageIndex)
+
+        if (isSuccess == true) {
+            increment()
+        }
+    }
+
+    private fun increment() {
+        pageIndex++
+    }
+
+    fun reset() {
+        pageIndex = START_PAGE_INDEX
+    }
+
+    companion object {
+        private const val START_PAGE_INDEX = 1
+    }
+
 }
