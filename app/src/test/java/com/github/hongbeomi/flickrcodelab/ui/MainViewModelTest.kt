8dd165f6b0240257e1e02c36a436520e4b8d7a45
hongbeom <dks3963@gmail.com>
2022-05-15 18:24:07 +0900

    MainViewModelTest에 에러 발생 시 테스트 코드 추가

--- app/src/test/java/com/github/hongbeomi/flickrcodelab/ui/MainViewModelTest.kt
+++ app/src/test/java/com/github/hongbeomi/flickrcodelab/ui/MainViewModelTest.kt
@@ -6,6 +6,7 @@ import com.github.hongbeomi.flickrcodelab.data.source.FakePhotoDataFactory
 import com.github.hongbeomi.flickrcodelab.data.source.FakePhotoListRepository
 import com.github.hongbeomi.flickrcodelab.data.source.remote.EXCEPTION_MESSAGE_LIST_EMPTY
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
@@ -15,6 +16,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@FlowPreview
 @ExperimentalCoroutinesApi
 class MainViewModelTest {
 
@@ -46,13 +48,28 @@ class MainViewModelTest {
     }
 
     @Test
+    fun givenError_WhenInitializeViewModel_ThenLoadingAndError() = runTest {
+        // given
+        repository.setReturnError(true)
+
+        // then
+        val result = viewModel.mainUiState.take(2).toList()
+
+        val loading = result.first() as MainViewModel.MainUiState.Loading
+        val error = result.last() as MainViewModel.MainUiState.Error
+
+        assertThat(loading, IsEqual(MainViewModel.MainUiState.Loading))
+        assertThat(error.exception.message, IsEqual("Test Exception"))
+    }
+
+    @Test
     fun givenNewPhotoList_WhenRefreshViewModel_ThenLoadingAndRemotePhotoList() = runTest {
         // given
         val remoteData = listOf(factory.photo1, factory.photo2, factory.photo3)
         repository.addPhotoList(*remoteData.toTypedArray())
 
         // when
-        viewModel.refresh(true)
+        viewModel.setUiAction(MainViewModel.MainUiAction.Refresh(true))
 
         // then
         val result = viewModel.mainUiState.take(2).toList()
@@ -71,17 +88,22 @@ class MainViewModelTest {
         repository.addPhotoList(*remoteData.toTypedArray())
 
         // loading, success
-        viewModel.refresh(true)
-        viewModel.loadMore()
+        viewModel.setUiAction(MainViewModel.MainUiAction.Refresh(true))
 
-        // then
-        val result = viewModel.mainUiState.drop(2).toList()
+        val result = viewModel.mainUiState.take(2).toList()
 
-        val loadingMore = result.first() as MainViewModel.MainUiState.LoadingMore
+        val loading = result.first() as MainViewModel.MainUiState.Loading
         val success = result.last() as MainViewModel.MainUiState.Success
 
-        assertThat(loadingMore, IsEqual(MainViewModel.MainUiState.LoadingMore))
-        assertThat(success.photoList, IsEqual(remoteData + remoteData))
+        assertThat(loading,  IsEqual(MainViewModel.MainUiState.Loading))
+        assertThat(success.photoList, IsEqual(remoteData))
+
+        // then
+        viewModel.setUiAction(MainViewModel.MainUiAction.LoadMore)
+
+        advanceUntilIdle()
+        val success1 = viewModel.mainUiState.value as MainViewModel.MainUiState.Success
+        assertThat(success1.photoList, IsEqual(remoteData + remoteData))
     }
 
 }
