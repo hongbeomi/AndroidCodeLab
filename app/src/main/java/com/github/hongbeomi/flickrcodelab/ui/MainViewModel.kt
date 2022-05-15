2909ccba62bf4da8e6a09884cabe364a56ac05a2
hongbeom <dks3963@gmail.com>
2022-05-15 18:21:10 +0900

    MainUiAction, Pager
    sealed class -> sealed interface로 변경, MainUiAction 인터페이스 선언, UiAction flow 선언, 페이저 구현

--- app/src/main/java/com/github/hongbeomi/flickrcodelab/ui/MainViewModel.kt
+++ app/src/main/java/com/github/hongbeomi/flickrcodelab/ui/MainViewModel.kt
@@ -4,19 +4,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.github.hongbeomi.flickrcodelab.data.source.PhotoListRepository
 import com.github.hongbeomi.flickrcodelab.model.Photo
+import com.github.hongbeomi.flickrcodelab.utils.Pager
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 
+@FlowPreview
 class MainViewModel(
     private val photoListRepository: PhotoListRepository
 ) : ViewModel() {
 
-    private val _mainUiState: MutableStateFlow<MainUiState> = MutableStateFlow(MainUiState.Loading)
-    val mainUiState: StateFlow<MainUiState> get() = _mainUiState
+    private val debounceTimeoutMillis = 1000L
 
+    private val _mainUiState: MutableStateFlow<MainUiState> = MutableStateFlow(MainUiState.Loading)
+    val mainUiState: StateFlow<MainUiState> = _mainUiState.asStateFlow()
+
+    private val mainUiAction: MutableSharedFlow<MainUiAction> = MutableSharedFlow()
+
+    private val pager = Pager()
     private val viewModelScope: CoroutineScope = CoroutineScope(
         SupervisorJob() + Dispatchers.Main.immediate
     )
@@ -25,40 +29,78 @@ class MainViewModel(
     }
 
     init {
-        refresh(isForceUpdate = true)
+        viewModelScope.launch(exceptionHandler) {
+            mainUiAction
+                .onEach {
+                    if (it is MainUiAction.Refresh) {
+                        _mainUiState.update { MainUiState.Loading }
+                    }
+                }
+                .debounce(debounceTimeoutMillis)
+                .collect {
+                    when (it) {
+                        is MainUiAction.LoadMore -> loadMore()
+                        is MainUiAction.Refresh -> refresh(it.isForceUpdate)
+                    }
+                }
+        }
+
+        refresh(true)
     }
 
-    fun refresh(isForceUpdate: Boolean = false) {
+    private fun refresh(isForceUpdate: Boolean) {
+        _mainUiState.update { MainUiState.Loading }
         viewModelScope.launch(exceptionHandler) {
-            _mainUiState.value = MainUiState.Loading
+            pager.reset()
+            pager.load {
+                photoListRepository
+                    .getAllPhotoList(isForceUpdate)
+                    .distinctUntilChanged()
+                    .collect { _mainUiState.value = MainUiState.Success(it) }
+                (_mainUiState.value as? MainUiState.Success)?.photoList?.isNotEmpty()
+            }
 
-            photoListRepository
-                .getAllPhotoList(isForceUpdate)
-                .distinctUntilChanged()
-                .collect { _mainUiState.value = MainUiState.Success(it) }
         }
     }
 
-    fun loadMore() {
+    private fun loadMore() {
         viewModelScope.launch(exceptionHandler) {
+            pager.load {
+                photoListRepository.loadMorePhotoList(it)
+            }
+        }
+    }
 
+    fun setUiAction(action: MainUiAction) {
+        viewModelScope.launch(exceptionHandler) {
+            mainUiAction.emit(action)
         }
     }
 
     override fun onCleared() {
+        pager.reset()
         viewModelScope.coroutineContext.cancel()
         super.onCleared()
     }
 
-    sealed class MainUiState {
-        class Success(val photoList: List<Photo> = emptyList()) : MainUiState()
-        object Loading : MainUiState()
-        object LoadingMore: MainUiState()
-        class Error(val exception: Throwable) : MainUiState()
+    sealed interface MainUiState {
+        data class Success(val photoList: List<Photo> = emptyList()) : MainUiState
+        object Loading : MainUiState
+        data class Error(val exception: Throwable) : MainUiState
+
+        fun isLoading() = this is Loading
+        fun isError() = this is Error
+        fun isSuccess() = this is Success
+    }
+
+    sealed interface MainUiAction {
+        data class Refresh(val isForceUpdate: Boolean) : MainUiAction
+        object LoadMore : MainUiAction
     }
 
 }
 
+@FlowPreview
 @Suppress("UNCHECKED_CAST")
 class MainViewModelFactory(
     private val photoListRepository: PhotoListRepository
